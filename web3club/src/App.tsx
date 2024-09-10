83821e75a4d2f15848da5ae44d932759104e184c
Gayathri P <p.gayathri.ch@gmail.com>
2024-09-10 10:10:17 +0530

    padding adjust

--- web3club/src/App.tsx
+++ web3club/src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Blog from './pages/Blog';
@@ -7,27 +8,16 @@ import News from './pages/News';
 import MintNFT from './pages/MintNFT';
 import Reviews from './pages/Reviews';
 import Digiboard from './pages/Digiboard';
-import NavigationBar from './components/NavigationBar'
+import NavigationBar from './components/NavigationBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@rainbow-me/rainbowkit/styles.css';
-import {
-  getDefaultConfig,
-  RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { WagmiProvider, ResolvedRegister } from 'wagmi';
-import {
-  mainnet,
-  polygon,
-  optimism,
-  arbitrum,
-  base,
-} from 'wagmi/chains';
-import {
-  QueryClientProvider,
-  QueryClient,
-} from "@tanstack/react-query";
+import { mainnet, polygon, optimism, arbitrum, base } from 'wagmi/chains';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import './App.css'; // Import your CSS file
+import './index.css'; // Import global CSS
 
-// RainbowKit and Wagmi config
 const config = getDefaultConfig({
   appName: 'My RainbowKit App',
   projectId: 'YOUR_PROJECT_ID',
@@ -37,27 +27,26 @@ const config = getDefaultConfig({
 });
 const queryClient = new QueryClient();
 
-// Main App component
-const App = () => {
+const App: React.FC = () => {
   return (
     <WagmiProvider config={config as unknown as ResolvedRegister['config']}>
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider>
           <Router>
             <div>
-              {/* Responsive Navbar */}
               <NavigationBar />
-              
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/blog" element={<Blog />} />
-                <Route path="/events-calendar" element={<EventsCalendar />} />
-                <Route path="/project-showcase" element={<ProjectShowcase />} />
-                <Route path="/news" element={<News />} />
-                <Route path="/mint-nft" element={<MintNFT />} />
-                <Route path="/reviews" element={<Reviews />} />
-                <Route path="/digiboard" element={<Digiboard />} />
-              </Routes>
+              <div className="page-content">
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/blog" element={<Blog />} />
+                  <Route path="/events-calendar" element={<EventsCalendar />} />
+                  <Route path="/project-showcase" element={<ProjectShowcase />} />
+                  <Route path="/news" element={<News />} />
+                  <Route path="/mint-nft" element={<MintNFT />} />
+                  <Route path="/reviews" element={<Reviews />} />
+                  <Route path="/digiboard" element={<Digiboard />} />
+                </Routes>
+              </div>
             </div>
           </Router>
         </RainbowKitProvider>
