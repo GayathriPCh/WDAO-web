1e8e320c07aa1d4d7d55fc6838cd74ce28ec8abb
Gayathri P <p.gayathri.ch@gmail.com>
2024-10-22 03:31:49 +0530

    extra

--- web3club/src/pages/IntroVid.tsx
+++ web3club/src/pages/IntroVid.tsx
@@ -4,8 +4,8 @@ import './IntroVid.css'; // Import the CSS file for styles
 const IntroVid: React.FC<{ videoSrc: string }> = ({ videoSrc }) => {
     return (
         <div className="video-container">
-            <video className="intro-video" controls>
-                <source src={videoSrc} type="video/mp4" />
+            <video className="intro-video" autoPlay loop muted>
+                <source src={videoSrc} type="video/webm" />
                 Your browser does not support the video tag.
             </video>
         </div>
